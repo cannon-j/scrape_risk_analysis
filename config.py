7fc5066cb6a3e9167fd9e16d09e6b30f7467225f
AxelTech <axeltech@AxelTechs-MacBook-Pro.local>
2016-02-08 22:32:22 -0500

    auth

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'aUTI81mhUdvluIOKVBo9875gi'
+consumer_secret = 'dm34YTPP9PUx514k2LGHBfFTqqXs5O0U5dO5Anb3BQV0vHCvfc'
+access_token = '1551852294-xVCLim2ia8NKf3iSZRTaER8VHOwLz7yRbWzLubZ'
+access_secret = 'E4aQEQJuOCSRcURUKA0bXCfZseEXL1g5UOFNbhMrHS8dM'
